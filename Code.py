e456a32e60264b22795ac73265b1fd2687cc350f
luca <lucabracaglia9908@gmail.com>
2021-11-30 17:11:14 +0100

    Implementazione codice

--- Code.py
+++ Code.py
@@ -13,8 +13,10 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
-#per allegerire il file abbiamo caricato da file csv
-taxi_df = pd.read_csv('./Data/yellow_tripdata_2021-05.csv')
+#per allegerire il file abbiamo caricato da file csv i primi 20 campi
+taxi_df = pd.read_csv('./Data/yellow_tripdata_2021-05.csv').head(20)
+
+zone_df = pd.read_csv('./Data/taxi_zone_lookup.csv')
 
 taxi_df.info()
 
@@ -23,9 +25,13 @@ taxi_df.rename(columns={
         'tpep_pickup_datetime': 'Inizio_corsa', 
         'tpep_dropoff_datetime': 'Fine_corsa'}, inplace=True)
 
+taxi_df["Inizio_corsa"].apply(lambda x: pd.Timestamp(x)).astype('int64') // 10**9
+taxi_df["Fine_corsa"].apply(lambda x: pd.Timestamp(x)).astype('int64') // 10**9
+
 #aggiungere ai campi vuoti di payment type il valore di unknown
 
 taxi_df['payment_type'].fillna(value='5.0', inplace = True)
+
 
 
 
