e0a4dea74e17f23c9a1f51391b44f7a6a315753d
Daniele <you@example.com>
2022-02-25 13:41:33 +0100

    Aggiunto il parser si permette di specificare mese, anno e borough di interesse. Si è snellito il main trasferendo porzioni di codice all interno di nuove funzioni.

--- main.py
+++ main.py
@@ -1,109 +1,59 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
-from datetime import datetime
 from Utils.corse import Corse
+import utilities
 
+# passo come parametri l' anno e il mese in cui voglio effettuare l' analisi 
+args = utilities.initializeParser()
 
-'''
-parser = Utils.initialize_parser()
-'''
+'''lettura dati'''
+taxi_df = utilities.lettura_dati(args.year, args.month)
+zone_df = pd.read_csv('Data/taxi_zone_lookup.csv', index_col="LocationID")
 
-
-"""Leggo tutto il file in ingresso attraverso un path relativo:"""
-
-
-
-taxi_df=pd.read_csv('Data/yellow_tripdata_2020-03.csv');
-#zone_df=pd.read_csv('Data/taxi+_zone_lookup.csv');
-zone_df=pd.read_csv('Data/taxi+_zone_lookup.csv', index_col="LocationID");
-
-"""verifico l'esistenza dei duplicato, la variabile è vuota se non esistono duplicati"""
-temp_df= taxi_df.drop_duplicates(inplace= True, keep = False)
-
-"""verifico l' esistenza di campi nulli, !DEVO DECIDERE SE ELIMINARLI O SOSTITUIRE IL VALORE MANCANTE:"""
-
-campi_vuoti= taxi_df.isnull().sum()
-TX=taxi_df.dropna()
-campi_vuoti1= TX.isnull().sum()
-taxi_df['VendorID'].fillna('1.0' , inplace= True)
-campi_vuoti2=taxi_df.isnull().sum()
-
-"""chiudo la variabile con tutti i record per liberare spazio in memoria e riapro il file prendendo 
-solo le prime 20 righe"""
-taxi_df = taxi_df.head(20)
-
-
-"""Rinomino le colonne del dataframe"""
-taxi_df.rename(columns={
-'tpep_pickup_datetime': 'inizio_corsa',
-'tpep_dropoff_datetime': 'fine_corsa',}, inplace=True)
+taxi_df=taxi_df.head(2000)
 
 '''si creano due series diverse:
     - una per l' inizio delle corse
-    - per la fine delle corse'''
+    - per la fine delle corse'''  
     
-
 inizio_corsa= taxi_df['inizio_corsa']
 fine_corsa= taxi_df['fine_corsa']
 zone= taxi_df['PULocationID']
 
-
-'''prima implementazione dell' algoritmo per calcolare la durata di un viaggio'''
-date_format = "%Y-%m-%d %H:%M:%S"
-
-conteggio=list()
-
-try:
-    i=0
-    while i<len(inizio_corsa):
-        a = datetime.strptime(inizio_corsa[i], date_format)
-        b = datetime.strptime(fine_corsa[i], date_format)
-        delta = b - a
-        delta = delta.total_seconds()
-        conteggio.append(delta)
-        i = i + 1
-   
-except:
-    print('Rilevati problemi con formattazione dati inserite (dd/mm/aaa)')
-
+'''calcolo le durate di tutti i viaggi'''
+conteggio=utilities.calcolo_durata(inizio_corsa, fine_corsa)
 
 '''
 calcolata la durata di ogni viaggio (conteggio), le inserisco in una series in modo da calcolarne la media e, in seguito creare un dataframe
 con tutte le corse che durano più della media.
  '''
 durata_corse = pd.Series(conteggio)
+taxi_df['durata_corse']=durata_corse
 media = durata_corse.mean()
 upper_quartile=np.percentile(durata_corse, 75)
 lower_quartile=np.percentile(durata_corse, 25)
 
 '''istanza delle diverse corse'''
-corsa_media = Corse(media, durata_corse, inizio_corsa, zone, zone_df)
 corsa_lunga = Corse(upper_quartile, durata_corse, inizio_corsa, zone, zone_df)
 corsa_breve = Corse(lower_quartile, durata_corse, inizio_corsa, zone, zone_df)
 
 '''crazione di due dizionari per la realizzazione di due dataframe'''
-data_upper= corsa_lunga.durata_corsa('upper')
-data_lower= corsa_breve.durata_corsa('lower')
+data_upper= corsa_lunga.durata_corsa('upper', args.borough)
+data_lower= corsa_breve.durata_corsa('lower', args.borough)
 
 
 '''il dataframe Corse lunghe contiene l'id zona, l'inizio della corsa e la durata del un viaggio'''
-
 Corse_lunghe = pd.DataFrame(data_upper)
 
 '''il dataframe Corse brevi contiene l'id zona, l'inizio della corsa e la durata del un viaggio'''
-
 Corse_brevi = pd.DataFrame(data_lower)
 
-'''il codice successivvo è qualora l analisi si volesse svolgere dalla media invece che dai quartili superiroe ed inf'''
-#data_mediaup= corsa_media.durata_corsa('upper')
-#data_medialow= corsa_media.durata_corsa('lower')
-#Corse_medieu = pd.DataFrame(data_mediaup)
-#Corse_mediel = pd.DataFrame(data_medialow)
-
+'''plot'''
+utilities.plot_corse(taxi_df, Corse_brevi, Corse_lunghe)
 
 
 '''salvataggio dei dati'''
 
 Corse_lunghe.to_csv('Output/Corse_lunghe.csv')
 Corse_brevi.to_csv('Output/Corse_brevi.csv')
+
